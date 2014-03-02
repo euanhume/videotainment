14ff7dd065a1be8da2331ff27ac1e66252cfc94f
Euan Hume <euan@euan-aspire.(none)>
2014-03-02 16:11:53 +0000

    if statement fix for live or dev

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
-
-if ($_SERVER["REMOTE_ADDR"]){
+$yii=dirname(__FILE__).'/../yii/framework/yii.php'; 
+if ($_SERVER["REMOTE_ADDR"] == '127.0.0.1'){
     $config=dirname(__FILE__).'/protected/config/development.php';
 }else{
     $config=dirname(__FILE__).'/protected/config/main.php';
